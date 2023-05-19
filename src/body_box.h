c317b75927a18a5c0e929e3dc261fdb56ad559a8
KaiN <kain@piwnica.ws>
2023-05-19 19:45:49 +0200

    added MPL license

--- src/body_box.h
+++ src/body_box.h
@@ -1,5 +1,9 @@
-#ifndef SLIPGATES_BODY_H
-#define SLIPGATES_BODY_H
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef SLIPGATES_BODY_BOX_H
+#define SLIPGATES_BODY_BOX_H
 
 #include <fixmath/fix16.h>
 #include <ace/managers/bob.h>
@@ -20,4 +24,4 @@ typedef struct tBodyBox {
 void bodySimulate(tBodyBox *pBody);
 void bodyInit(tBodyBox *pBody, fix16_t fPosX, fix16_t fPosY, UBYTE ubWidth, UBYTE ubHeight);
 
-#endif // SLIPGATES_BODY_H
+#endif // SLIPGATES_BODY_BOX_H
